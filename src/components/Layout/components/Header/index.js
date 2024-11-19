b7b5d30eb40ae97a9e52dbc716e54822e088de1d
Dinh Long <trandoandinhlong@gmail.com>
2024-11-19 17:36:53 +0700

    Bài 98 Xây dựng UI phần Header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -9,6 +9,7 @@ import {
 import Tippy from '@tippyjs/react/headless';
 
 //---------------------------------
+import Button from '~/components/Button';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
@@ -59,7 +60,7 @@ function Header() {
                         </button>
                         <FontAwesomeIcon
                             className={cx('loading')}
-                            icon={faSpinner}
+                            icos={faSpinner}
                         />
 
                         <button className={cx('search-btn')}>
@@ -67,7 +68,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('action')}></div>
+                <div className={cx('action')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
