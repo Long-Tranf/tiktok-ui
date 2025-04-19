2a0905fc86a5b8ef43f4da7bda158e5a92216c0f
Dinh Long <trandoandinhlong@gmail.com>
2025-04-19 12:52:03 +0700

    Xây dựng UI phần Header #2

---
+++ src/Components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                alt="Avatar"
+                src="https://i.pinimg.com/236x/10/2d/70/102d7081596cf61051ad81274cf0169e.jpg"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
